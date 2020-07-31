2310da79b2c14aa2ed311c9501f96efef585ca07
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-07-31 07:24:27 +0300

    change form pages on click

--- js/script.js
+++ js/script.js
@@ -6,4 +6,23 @@ $(document).ready(function () {
   $('.actions__button__submit').addClass('actions__button--hidden');
 
   $('.actions__button__back').css('visibility', 'hidden');
+
+  $('.actions__button__next').click(function () {
+    $('.contact-form__body__inputs--second-page').removeClass(
+      'contact-form__body__inputs--hidden'
+    );
+    $('.contact-form__body__inputs--first-page').addClass(
+      'contact-form__body__inputs--hidden'
+    );
+
+    $('.contact-form__body__badge--current').text(function () {
+      return 'Block 2';
+    });
+
+    $('.actions__button__submit').removeClass('actions__button--hidden');
+
+    $('.actions__button__next').css('visibility', 'hidden');
+
+    $('.actions__button__back').css('visibility', '');
+  });
 });
