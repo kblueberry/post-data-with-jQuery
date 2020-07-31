50d2b99f07f0d8bfb947e9d457fd68c3792e7d8b
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-07-31 07:01:42 +0300

    hided buttons on document loaded

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.contact-form__body__inputs--second-page').addClass(
+    'contact-form__body__inputs--hidden'
+  );
+
+  $('.actions__button__submit').addClass('actions__button--hidden');
+
+  $('.actions__button__back').css('visibility', 'hidden');
+});
