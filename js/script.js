8b247068d597a804333bfb6871437cfe2e38d7b1
Khrystyna Derenivska <khrisderenivska@gmail.com>
2020-07-31 22:08:17 +0300

    file info block paddings and flex

--- js/script.js
+++ js/script.js
@@ -38,9 +38,7 @@ $(document).ready(function () {
 
     $('.actions__button__back').css('visibility', 'hidden');
 
-    $('.contact-form__body__badge--current').text(function () {
-      return 'Block 1';
-    });
+    $('.contact-form__body__badge--current').text('Block 1');
   });
 
   //   let requiredInputs = [];
@@ -53,14 +51,24 @@ $(document).ready(function () {
     $('#input-upload').click();
   });
 
-  $('#input-upload').bind('change', function () {
-    //this.files[0].size gets the size of your file.
-    alert(this.files[0].size);
+  $('#input-upload').change(function () {
+    const file = this.files[0];
+    $('#file-name').text(file.name);
+    $('#file-info').text(
+      `${formatSize(file.size)} | ${formatDate(file.lastModified)}`
+    );
   });
 
   $('#input-upload').change(function () {
     $('#attached-files').css('display', '');
     $('#file-upload-info').css('display', 'none');
+  });
+
+  $('#attached-files__close').click(function (e) {
+    e.preventDefault();
+    $('#attached-files').css('display', 'none');
+    $('#file-upload-info').css('display', '');
+    $('#input-upload').val(null);
   });
 });
 
@@ -79,4 +87,27 @@ function validate() {
     let validationMessage = $('.form-field__invalid-message');
     validationMessage.removeClass('form-field__invalid-message--hidden');
   }
+}
+
+function formatSize(sizeBytes) {
+  const bytesInMb = 1024 * 1024;
+  const isGreaterThanOneMb = sizeBytes >= bytesInMb;
+  if (isGreaterThanOneMb) {
+    return `${formatNumber(sizeBytes / bytesInMb)} Mb`;
+  } else {
+    return `${formatNumber(sizeBytes / 1024)} kb`;
+  }
+}
+
+function formatNumber(n) {
+  return n.toLocaleString('en-US');
+}
+
+function formatDate(epoch) {
+  const date = new Date(epoch);
+  return date.toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  });
 }
